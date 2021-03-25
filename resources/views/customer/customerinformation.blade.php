81bf47c72e0ad819a763cd288be6e47464fb449d
Farras Naim <71861102+farrasnaim@users.noreply.github.com>
2021-03-25 19:43:42 +0700

    beautifying code

--- resources/views/customer/customerinformation.blade.php
+++ resources/views/customer/customerinformation.blade.php
@@ -36,9 +36,9 @@
     </div>
 
     <div class="wrapper">
-    <!-- end loader -->
+        <!-- end loader -->
 
-     <div class="sidebar">
+        <div class="sidebar">
             <!-- Sidebar  -->
             <nav id="sidebar">
 
@@ -48,7 +48,7 @@
 
                 <ul class="list-unstyled components">
 
-                    <li >
+                    <li>
                         <a href="index.html">Home</a>
                     </li>
                     <li>
@@ -74,117 +74,135 @@
             </nav>
         </div>
 
-    <div id="content">
-    <!-- header -->
-    <header>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="full">
-                        <a class="logo" href="index.html"><img src="{{URL::to('/')}}/informationassets/images/title.png" alt="DAPUR SYADIT" /></a>
-                    </div>
-                </div>
-                <div class="col-md-9">
-                    <div class="full">
-                        <div class="right_header_info">
-                            <ul>
-                                <li class="dinone">Contact Us : <img style="margin-right: 15px;margin-left: 15px;" src="{{URL::to('/')}}/informationassets/images/phone_icon.png" alt="#"><a href="#">666</a></li>
-                                <li class="dinone"><img style="margin-right: 15px;" src="{{URL::to('/')}}/informationassets/images/mail_icon.png" alt="#"><a href="#">dapursyadit@gmail.com</a></li>
-                                <li class="dinone"><img style="margin-right: 15px;height: 21px;position: relative;top: -2px;" src="{{URL::to('/')}}/informationassets/images/location_icon.png" alt="#"><a href="#">Tegal, Jawa Tengah</a></li>
-                                <li class="button_user"><a class="button active" href="#">Login</a><a class="button" href="#">Register</a></li>
-                                <li><img style="margin-right: 15px;" src="{{URL::to('/')}}/informationassets/images/search_icon.png" alt="#"></li>
-                                <li>
-                                    <button type="button" id="sidebarCollapse">
-                                        <img src="{{URL::to('/')}}/informationassets/images/menu_icon.png" alt="#">
-                                    </button>
-                                </li>
-                            </ul>
+        <div id="content">
+            <!-- header -->
+            <header>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="full">
+                                <a class="logo" href="index.html"><img
+                                        src="{{URL::to('/')}}/informationassets/images/title.png"
+                                        alt="DAPUR SYADIT" /></a>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
-    <!-- end header -->
-    <!-- footer -->
-    <fooetr>
-        <div class="footer">
-            <div class="container-fluid">
-                <div class="row">
-                  <div class=" col-md-12">
-                    <h2>Customer<strong class="white"> Information</strong></h2>
-                  </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                      
-                        <form class="main_form">
-                            <div class="row">
-                             
-                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                                    <input class="form-control" placeholder="Name" type="text" name="Name">
-                                </div>
-                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                                    <input class="form-control" placeholder="Email" type="text" name="Email">
-                                </div>
-                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                                    <input class="form-control" placeholder="Phone" type="text" name="Phone">
-                                </div>
-                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                                    <input class="form-control" placeholder="Address" type="text" name="Address">
-                                </div>
-                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                                    <textarea class="textarea" placeholder="Description" type="text" name="Description"></textarea>
-                                </div>
-                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                                    <button class="send">Send</button>
+                        <div class="col-md-9">
+                            <div class="full">
+                                <div class="right_header_info">
+                                    <ul>
+                                        <li class="dinone">Contact Us : <img
+                                                style="margin-right: 15px;margin-left: 15px;"
+                                                src="{{URL::to('/')}}/informationassets/images/phone_icon.png"
+                                                alt="#"><a href="#">666</a></li>
+                                        <li class="dinone"><img style="margin-right: 15px;"
+                                                src="{{URL::to('/')}}/informationassets/images/mail_icon.png" alt="#"><a
+                                                href="#">dapursyadit@gmail.com</a></li>
+                                        <li class="dinone"><img
+                                                style="margin-right: 15px;height: 21px;position: relative;top: -2px;"
+                                                src="{{URL::to('/')}}/informationassets/images/location_icon.png"
+                                                alt="#"><a href="#">Tegal, Jawa Tengah</a></li>
+                                        <li class="button_user"><a class="button active" href="#">Login</a><a
+                                                class="button" href="#">Register</a></li>
+                                        <li><img style="margin-right: 15px;"
+                                                src="{{URL::to('/')}}/informationassets/images/search_icon.png" alt="#">
+                                        </li>
+                                        <li>
+                                            <button type="button" id="sidebarCollapse">
+                                                <img src="{{URL::to('/')}}/informationassets/images/menu_icon.png"
+                                                    alt="#">
+                                            </button>
+                                        </li>
+                                    </ul>
                                 </div>
                             </div>
-                        </form>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                        <div class="img-box">
-                            <figure><img src="{{URL::to('/')}}/informationassets/images/catering.jpeg" alt="img" /></figure>
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="footer_logo">
-                          <a href="index.html"><img src="{{URL::to('/')}}/informationassets/images/logo1.jpg" alt="logo" /></a>
-                        </div>
-                    </div>
-                    <div class="col-md-12">
-                        <ul class="lik">
-                            <li > <a href="index.html">Home</a></li>
-                            <li > <a href="index.html">Order</a></li>
-                            <li > <a href="index.html">History</a></li>
-                            <li> <a href="recipe.html">Menu</a></li>
-                            <li> <a href="about.html">About</a></li>
-                            <li class="active"> <a href="contact.html">Contact us</a></li>
-                            <li> <a href="about.html">FAQ</a></li>
+            </header>
+            <!-- end header -->
+            <!-- footer -->
+            <fooetr>
+                <div class="footer">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class=" col-md-12">
+                                <h2>Customer<strong class="white"> Information</strong></h2>
+                            </div>
+                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
 
-                        </ul>
+                                <form class="main_form">
+                                    <div class="row">
+
+                                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                                            <input class="form-control" placeholder="Name" type="text" name="Name">
+                                        </div>
+                                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                                            <input class="form-control" placeholder="Email" type="text" name="Email">
+                                        </div>
+                                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                                            <input class="form-control" placeholder="Phone" type="text" name="Phone">
+                                        </div>
+                                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                                            <input class="form-control" placeholder="Address" type="text"
+                                                name="Address">
+                                        </div>
+                                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                                            <textarea class="textarea" placeholder="Description" type="text"
+                                                name="Description"></textarea>
+                                        </div>
+                                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                                            <button class="send">Send</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                                <div class="img-box">
+                                    <figure><img src="{{URL::to('/')}}/informationassets/images/catering.jpeg"
+                                            alt="img" /></figure>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="footer_logo">
+                                    <a href="index.html"><img src="{{URL::to('/')}}/informationassets/images/logo1.jpg"
+                                            alt="logo" /></a>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <ul class="lik">
+                                    <li> <a href="index.html">Home</a></li>
+                                    <li> <a href="index.html">Order</a></li>
+                                    <li> <a href="index.html">History</a></li>
+                                    <li> <a href="recipe.html">Menu</a></li>
+                                    <li> <a href="about.html">About</a></li>
+                                    <li class="active"> <a href="contact.html">Contact us</a></li>
+                                    <li> <a href="about.html">FAQ</a></li>
+
+                                </ul>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="new">
+                                    <h3>Newsletter</h3>
+                                    <form class="newtetter">
+                                        <input class="tetter" placeholder="Your email" type="text" name="Your email">
+                                        <button class="submit">Subscribe</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-md-12">
-                        <div class="new">
-                            <h3>Newsletter</h3>
-                            <form class="newtetter">
-                                <input class="tetter" placeholder="Your email" type="text" name="Your email">
-                                <button class="submit">Subscribe</button>
-                            </form>
+                    <div class="copyright">
+                        <div class="container">
+                            <p></a>Dapur Syadit 2021</p>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="copyright">
-                <div class="container">
-                    <p></a>Dapur Syadit 2021</p>
-                </div>
-            </div>
+            </fooetr>
+            <!-- end footer -->
+
         </div>
-    </fooetr>
-    <!-- end footer -->
-
-    </div>
     </div>
     <div class="overlay"></div>
     <!-- Javascript files-->
@@ -193,27 +211,28 @@
     <script src="{{URL::to('/')}}/informationassets/js/bootstrap.bundle.min.js"></script>
     <script src="{{URL::to('/')}}/informationassets/js/owl.carousel.min.js"></script>
     <script src="{{URL::to('/')}}/informationassets/js/custom.js"></script>
-     <script src="{{URL::to('/')}}/informationassets/js/jquery.mCustomScrollbar.concat.min.js"></script>
-    
-     <script src="js/jquery-3.0.0.min.js"></script>
-   <script type="text/javascript">
-        $(document).ready(function() {
+    <script src="{{URL::to('/')}}/informationassets/js/jquery.mCustomScrollbar.concat.min.js"></script>
+
+    <script src="js/jquery-3.0.0.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
             $("#sidebar").mCustomScrollbar({
                 theme: "minimal"
             });
 
-            $('#dismiss, .overlay').on('click', function() {
+            $('#dismiss, .overlay').on('click', function () {
                 $('#sidebar').removeClass('active');
                 $('.overlay').removeClass('active');
             });
 
-            $('#sidebarCollapse').on('click', function() {
+            $('#sidebarCollapse').on('click', function () {
                 $('#sidebar').addClass('active');
                 $('.overlay').addClass('active');
                 $('.collapse.in').toggleClass('in');
                 $('a[aria-expanded=true]').attr('aria-expanded', 'false');
             });
         });
+
     </script>
 
 </body>
