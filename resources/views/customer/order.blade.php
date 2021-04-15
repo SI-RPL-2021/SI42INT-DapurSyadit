3f9f384ff578344dcdfa7582a477b62ee182de95
Farras Naim <farrasnaim@icloud.com>
2021-04-15 22:17:22 +0700

    update codes

--- resources/views/customer/order.blade.php
+++ resources/views/customer/order.blade.php
@@ -1,310 +1,152 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <!-- basic -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- mobile metas -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
-    <!-- site metas -->
-    <title>Order</title>
-    <meta name="keywords" content="">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <!-- bootstrap css -->
-    <link rel="stylesheet" href="{{asset ('/customerasset/css/bootstrap.min.css') }}">
-    <!-- owl css -->
-    <link rel="stylesheet" href="{{asset ('/customerasset/css/owl.carousel.min.css') }}">
-    <!-- style css -->
-    <link rel="stylesheet" href="{{asset ('/customerasset/css/style.css') }}">
-    <!-- responsive-->
-    <link rel="stylesheet" href="{{asset ('/customerasset/css/responsive.css') }}">
-    <!-- awesome fontfamily -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Admin Product</title>
+
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+  <!-- CSS Libraries -->
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{asset ('/adminassets/css/style.css') }}">
+  <link rel="stylesheet" href="{{asset ('/adminassets/css/components.css') }}">
 </head>
-<!-- body -->
 
-<body class="main-layout">
-    <!-- loader  -->
-    <div class="loader_bg">
-        <div class="loader"><img src="images/loading.gif" alt="" /></div>
-    </div>
-
-    <div class="wrapper">
-    <!-- end loader -->
-
-     <div class="sidebar">
-            <!-- Sidebar  -->
-            <nav id="sidebar">
-
-                <div id="dismiss">
-                    <i class="fa fa-arrow-left"></i>
+<body>
+  <div id="app">
+    <div class="main-wrapper">
+      <div class="navbar-bg"></div>
+      <nav class="navbar navbar-expand-lg main-navbar">
+        <form class="form-inline mr-auto">
+          <ul class="navbar-nav mr-3">
+            <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+            <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
+          </ul>
+         
+        </form>
+        <ul class="navbar-nav navbar-right">
+          <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class=""><i class="far fa-envelope"></i></a>
+            <div class="dropdown-menu dropdown-list dropdown-menu-right">
+            
+                <div class="float-right">
+               
                 </div>
-
-                <ul class="list-unstyled components">
-
-                    <li class="active">
-                        <a href="#">Home</a>
-                    </li>
-                    <li>
-                        <a href="#">Menu</a>
-                    </li>
-                    <li>
-                        <a href="#">Order</a>
-                    </li>
-                    <li>
-                        <a href="#">FAQ</a>
-                    </li>
-                    <li>
-                        <a href="#">About</a>
-                    </li>
+              </div>
+              
+          </li>
+          <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class=""><i class="far fa-bell"></i></a>
+            <div class="dropdown-menu dropdown-list dropdown-menu-right">
+        
+                <div class="float-right">
+                  
+                </div>
+              </div>
+              
+          </li>
+          <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+            <img alt="" src="" class="rounded-circle mr-1">
+            <div class="d-sm-none d-lg-inline-block">Hi, Dzaki Rachmanda</div></a>
+            <div class="dropdown-menu dropdown-menu-right">
+            
+              <div class="dropdown-divider"></div>
+              <a href="#" class="dropdown-item has-icon text-danger">
+                <i class="fas fa-sign-out-alt"></i> Logout
+              </a>
+            </div>
+          </li>
+        </ul>
+      </nav>
+      <div class="main-sidebar">
+        <aside id="sidebar-wrapper">
+          <div class="sidebar-brand">
+            <a href="index.html">Dapoer Syadit</a>
+          </div>
+          <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+          </div>
+          <ul class="sidebar-menu">
+              <li class="menu-header">Dashboard</li>
+              <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                <ul class="dropdown-menu">
+                  <li><a class="nav-link" href="index-0.html">General Dashboard</a></li>
+                  <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
                 </ul>
-
-            </nav>
-        </div>
-
-    <div id="content">
-    <!-- header -->
-    <header>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="full">
-                        <a class="logo" href="index.html"><img src="images/logo.png" alt="#" /></a>
-                    </div>
-                </div>
-                <div class="col-md-9">
-                    <div class="full">
-                        <div class="right_header_info">
-                            <ul>
-                                <li class="dinone">MENU<img style="margin-right: 15px;margin-left: 15px;" src="{{asset ('/customerasset/images/phone_icon.png') }}" alt="#"><a href="#">MENU</a></li>
-                                <li class="dinone"><img style="margin-right: 15px;" src="{{asset ('/customerasset/images/mail_icon.png') }}" alt="#"><a href="#">ORDER</a></li>
-                                <li class="dinone"><img style="margin-right: 15px;height: 21px;position: relative;top: -2px;" src="{{asset ('/customerasset/images/location_icon.png') }}" alt="#"><a href="#">FAQ</a></li>
-                                <li class="button_user"><a class="button active" href="#">Login</a><a class="button" href="#">Register</a></li>
-                                <li><img style="margin-right: 15px;" src="{{asset ('/customerasset/images/search_icon.png') }}" alt="#"></li>
-                                <li>
-                                    <button type="button" id="sidebarCollapse">
-                                        <img src="{{asset ('/customerasset/images/menu_icon.png') }}" alt="#">
-                                    </button>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
-    <!-- end header -->
-    <!-- section -->
-    <section class="resip_section">
-        <div class="container">
-            <div class="row">
-         <div class="col-md-12">
-       <div class="ourheading">
-    <h2>Our Menu</h2>
-  </div>
-</div>    
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="owl-carousel owl-theme">
-
-                <div class="item">
-                    <div class="product_blog_img">
-                        <img src="{{asset ('/customerasset/images/rs1.png') }}" alt="#" />
-                    </div>
-                    <div class="product_blog_cont">
-                        <h3>PAKET A</h3>
-                        <h4><span class="theme_color">$</span>10</h4>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="product_blog_img">
-                        <img src="{{asset ('/customerasset/images/rs2.png') }}" alt="#" />
-                    </div>
-                    <div class="product_blog_cont">
-                        <h3>PAKET B</h3>
-                        <h4><span class="theme_color">$</span>20</h4>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="product_blog_img">
-                        <img src="{{asset ('/customerasset/images/rs3.png') }}" alt="#" />
-                    </div>
-                    <div class="product_blog_cont">
-                        <h3>PAKET C</h3>
-                        <h4><span class="theme_color">$</span>30</h4>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="product_blog_img">
-                        <img src="{{asset ('/customerasset/images/rs4.png') }}" alt="#" />
-                    </div>
-                    <div class="product_blog_cont">
-                        <h3>PAKET D</h3>
-                        <h4><span class="theme_color">$</span>40</h4>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="product_blog_img">
-                        <img src="{{asset ('/customerasset/images/rs5.png') }}" alt="#" />
-                    </div>
-                    <div class="product_blog_cont">
-                        <h3>PAKET E</h3>
-                        <h4><span class="theme_color">$</span>50</h4>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="product_blog_img">
-                        <img src="{{asset ('/customerasset/images/rs1.png') }}" alt="#" />
-                    </div>
-                    <div class="product_blog_cont">
-                        <h3>PAKET F</h3>
-                        <h4><span class="theme_color">$</span>10</h4>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="product_blog_img">
-                        <img src="{{asset ('/customerasset/images/rs2.png')}}" alt="#" />
-                    </div>
-                    <div class="product_blog_cont">
-                        <h3>PAKET G</h3>
-                        <h4><span class="theme_color">$</span>20</h4>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="product_blog_img">
-                        <img src="{{asset ('/customerasset/images/rs3.png') }}" alt="#" />
-                    </div>
-                    <div class="product_blog_cont">
-                        <h3>PAKET H</h3>
-                        <h4><span class="theme_color">$</span>30</h4>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="product_blog_img">
-                        <img src="{{asset ('/customerasset/images/rs4.png') }}" alt="#" />
-                    </div>
-                    <div class="product_blog_cont">
-                        <h3>PAKET I</h3>
-                        <h4><span class="theme_color">$</span>40</h4>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="product_blog_img">
-                        <img src="{{asset ('/customerasset/images/rs5.png') }}" alt="#" />
-                    </div>
-                    <div class="product_blog_cont">
-                        <h3>PAKET J</h3>
-                        <h4><span class="theme_color">$</span>50</h4>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
-</div>
-</div>
-</div>
-</section>
-
-    <!-- footer -->
-    <footer>
-        <div class="footer">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-md-12">
-                        <ul class="lik">
-                            <li class="active"> <a href="index.html">Home</a></li>
-                            <li> <a href="about.html">About</a></li>
-                            <li> <a href="recipe.html">Recipe</a></li>
-                            <li> <a href="blog.html">Blog</a></li>
-                            <li> <a href="contact.html">Contact us</a></li>
-                        </ul>
-                    </div>
-                    </div>
-                </div>
-            </div>
-            <div class="copyright">
-                <div class="container">
-                    <p>© 2019 All Rights Reserved. Design by<a href="https://html.design/"> Dapur Syadit</a></p>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!-- end footer -->
-
-    </div>
-    </div>
-    <div class="overlay"></div>
-    <!-- Javascript files-->
-    <script src="{{asset ('/customerasset/js/jquery.min.js') }}"></script>
-    <script src="{{asset ('/customerasset/js/popper.min.js') }}"></script>
-    <script src="{{asset ('/customerasset/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{asset ('/customerasset/js/owl.carousel.min.js') }}"></script>
-    <script src="{{asset ('/customerasset/js/custom.js') }}"></script>
-     <script src="{{asset ('/customerasset/js/jquery.mCustomScrollbar.concat.min.js') }}"></script>
+              </li>
+              <li class="menu-header">Starter</li>
+              <li class="nav-item dropdown active">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Product</span></a>
+                <ul class="dropdown-menu">
+                  <li class="active"><a class="nav-link" href="layout-default.html">Product</a></li>
+                </ul>
+              </li>
     
-     <script src="{{asset ('/customerasset/js/jquery-3.0.0.min.js') }}"></script>
-   <script type="text/javascript">
-        $(document).ready(function() {
-            $("#sidebar").mCustomScrollbar({
-                theme: "minimal"
-            });
+           
+            </div>
+        </aside>
+      </div>
 
-            $('#dismiss, .overlay').on('click', function() {
-                $('#sidebar').removeClass('active');
-                $('.overlay').removeClass('active');
-            });
+      <!-- Main Content -->
+      <div class="main-content">
+        <section class="section">
+          <div class="section-header">
+            <h1>Dapoer Syadit Product</h1>
+            <div class="section-header-breadcrumb">
+              <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+              <div class="breadcrumb-item"><a href="#">Product</a></div>
+              <div class="breadcrumb-item">Dapoer Syadit Product</div>
+            </div>
+          </div>
+          
+          <div class="section-body">
+            <h2 class="section-title">This is Dapoer Syadit Product</h2>
+            <p class="section-lead">Dapoer Syadit Product</p>
+            <div class="card">
+						<div class="col-md-12">
+							<div class="card">
+								<div class="card-header">
+									<a href="" class="btn btn-primary btn-round ml-auto" style="float: right;">
+										<i class="fa fa-plus"></i>
+										Add Product
+									</a>
+								</div>
+                <div class="card-body">
+									<div class="table-responsive">
+										<table id="paket-datatables" class="display table table-bordered table-hover"  >
+											<thead>
+												<tr>
+                          <th rowspan="2">Id</th>
+													<th rowspan="2">Name</th>
+													<th rowspan="2">Price</th>
+													<th rowspan="2">Product Description</th>
+													<th rowspan="2">Action</th>
+                        </tr>
+                        <td>
+														<li class="list-inline-item">
+															<a href="" class="btn btn-primary btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-edit"></i></a>
+															<a href="" class="btn btn-info btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-trash"></i></a>
+														</li>
+													</td>
+            
 
-            $('#sidebarCollapse').on('click', function() {
-                $('#sidebar').addClass('active');
-                $('.overlay').addClass('active');
-                $('.collapse.in').toggleClass('in');
-                $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-            });
-        });
-    </script>
+  <!-- General JS Scripts -->
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+  <script src="../assets/js/stisla.js"></script>
 
-    <style>
-    #owl-demo .item{
-        margin: 3px;
-    }
-    #owl-demo .item img{
-        display: block;
-        width: 100%;
-        height: auto;
-    }
-    </style>
+  <!-- JS Libraies -->
 
-     
-      <script>
-         $(document).ready(function() {
-           var owl = $('.owl-carousel');
-           owl.owlCarousel({
-             margin: 10,
-             nav: true,
-             loop: true,
-             responsive: {
-               0: {
-                 items: 1
-               },
-               600: {
-                 items: 2
-               },
-               1000: {
-                 items: 5
-               }
-             }
-           })
-         })
-      </script>
+  <!-- Template JS File -->
+  <script src="../assets/js/scripts.js"></script>
+  <script src="../assets/js/custom.js"></script>
 
+  <!-- Page Specific JS File -->
 </body>
-
 </html>
