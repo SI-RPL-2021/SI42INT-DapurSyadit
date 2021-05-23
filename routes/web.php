2f16a45d0872da21f33d08894ece285d4767e051
Farras Naim <farrasnaim@student.telkomuniversity.ac.id>
2021-05-23 20:33:33 +0700

    fixing routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Products;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 // Landing Page
 Route::get('/', function () {
-    return view('welcome');
+    $products = Products::all();
+    return view('welcome', compact('products'));
 })->name('welcome');
 
 // AUTH
@@ -26,6 +28,8 @@ Route::get('/home', [App\Http\Controllers\OrdersController::class, 'index'])->na
 Route::get('/order/{id}', [App\Http\Controllers\OrdersController::class, 'create'])->name('create');
 Route::post('/order', [App\Http\Controllers\OrdersController::class, 'createProcess'])->name('createProcess');
 Route::get('/transaction', [App\Http\Controllers\OrdersController::class, 'show'])->name('transaction');
+Route::get('/orderList', [App\Http\Controllers\OrdersController::class, 'orderList'])->name('orderList');
+Route::post('/orderList', [App\Http\Controllers\OrdersController::class, 'UorderDelete'])->name('UorderDelete');
 
 // ADMIN
 Route::get('admin/home', [App\Http\Controllers\ProductsController::class, 'index'])->name('admin.home')->middleware('is_admin');
@@ -36,3 +40,6 @@ Route::post('admin/update/{id}', [App\Http\Controllers\ProductsController::class
 Route::post('admin/delete', [App\Http\Controllers\ProductsController::class, 'destroy'])->name('admin.delete')->middleware('is_admin');
 Route::get('admin/user', [App\Http\Controllers\ProductsController::class, 'user'])->name('admin.user')->middleware('is_admin');
 Route::post('admin/userDelete', [App\Http\Controllers\ProductsController::class, 'destroyUser'])->name('admin.userDelete')->middleware('is_admin');
+Route::get('admin/userOrder', [App\Http\Controllers\OrdersController::class, 'userOrder'])->name('admin.userOrder')->middleware('is_admin');
+Route::post('admin/userOrder', [App\Http\Controllers\OrdersController::class, 'orderProcess'])->name('admin.orderProcess')->middleware('is_admin');
+Route::post('admin/orderDelete', [App\Http\Controllers\OrdersController::class, 'orderDelete'])->name('admin.orderDelete')->middleware('is_admin');
