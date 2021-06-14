989aff7f43a9b8c8a16ae0cb0db2369d8a5faa31
mrafidzaky <mrafidzaky@student.telkomuniversity.ac.id>
2021-06-14 20:18:01 +0700

    resolved #16

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -64,7 +64,7 @@
                         <div class="card-header" id="headingOne">
                             <h5 class="mb-0">
                                 <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                    What is Dapur Syadit
+                                    What is Dapur Syadit?
                                 </button>
                             </h5>
                         </div>
